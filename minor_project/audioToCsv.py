aec3f67c97d04ef2906efaa07539ad504b5e9ef4
siddharthjoharwal <47102437+siddharthjoharwal@users.noreply.github.com>
2020-08-24 10:14:40 +0530

    Add files via upload

---
+++ minor_project/audioToCsv.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 15:04:20 2020
+
+@author: siddharth
+"""
+
+import librosa
+import librosa.display
+import os
+import csv
+import numpy as np
+
+header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+for i in range(1, 21):
+    header += f' mfcc{i}'
+header += ' label'
+header = header.split()
+
+file = open('Data.csv', 'w', newline='')
+with file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+    
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+for g in genres:
+    for filename in os.listdir(f'genres/{g}'):
+        songname = f'genres/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=30)
+        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+        rmse = librosa.feature.chroma_stft(y=y,sr=sr)
+        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        zcr = librosa.feature.zero_crossing_rate(y)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr)
+        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+        for e in mfcc:
+            to_append += f' {np.mean(e)}'
+        to_append += f' {g}'
+        file = open('Data.csv', 'a', newline='')
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(to_append.split())
